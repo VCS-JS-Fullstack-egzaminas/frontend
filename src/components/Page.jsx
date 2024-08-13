715fd34e737d5cb41748575279d5bf3ce68da266
dgenutis <dainiusgenutis@yahoo.com>
2024-08-13 18:43:51 +0300

    papildytas pagrindinis page info ir flipu

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,204 +1,357 @@
-import './Page.css'
+import { useState } from "react";
+import "./Page.css";
+
+const cars = [
+  {
+    id: 1,
+    imgSrc:
+      "../../public/auto/Automobilio-nuoma-Toyota-Corolla-autonuoma-Vilniuje3-265x128.jpg",
+    title: "Toyota Corolla",
+    size: "Vidutinė (IDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 21.56€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 2,
+    imgSrc: "../../public/auto/Automobilio-nuoma-Volkswagen-Golf3-265x151.png",
+    title: "Volkswagen Golf 3",
+    size: "Kompaktinė (CDMR)",
+    year: "metai: 2020",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 18.78€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 3,
+    imgSrc: "../../public/auto/Automobilio-nuoma-Volkswagen-Jetta-265x132.png",
+    title: "Volkswagen Jetta",
+    size: "Vidutinė (IDMR)",
+    year: "metai: 2020",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 21.88€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 4,
+    imgSrc: "../../public/auto/Automobilio-nuoma-Volkswagen-Polo-265x149.png",
+    title: "Volkswagen Polo",
+    size: "Ekonominė",
+    year: "metai: 2021",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 17,67€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 5,
+    imgSrc: "../../public/auto/Automobilių-nuoma-Ford-Focus3-265x144.png",
+    title: "Ford Focus",
+    size: "Kompaktinė (CWAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 17.9€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 6,
+    imgSrc: "../../public/auto/Automobilių-nuoma-Ford-Mondeo.-265x153.png",
+    title: "Ford Mondeo",
+    size: "Standartinė (SDAR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: Automatinė",
+    price: "Kaina nuo: 29.22€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 7,
+    imgSrc: "../../public/auto/Automobilių-nuoma-Hyundai-i10-252x160.jpg",
+    title: "Hyundai i10",
+    size: "Mini (MDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 9.98€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 8,
+    imgSrc: "../../public/auto/hyundai-i20.png",
+    title: "Hyundai i20",
+    size: "Ekonominė",
+    year: "metai: 2021",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 22.45€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 9,
+    imgSrc: "../../public/auto/Automobiliu-nuoma-Hyundai-i30.-265x138.jpg",
+    title: "Hyundai i30",
+    size: "Kompaktinė (CDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 18.88€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 10,
+    imgSrc:
+      "../../public/auto/Automobiliu-nuoma-Toyota-Auris-autonuoma..-265x150.jpg",
+    title: "Toyota Auris",
+    size: "Kompaktinė (CDMR)",
+    year: "metai: 2019",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 19.77€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 11,
+    imgSrc:
+      "../../public/auto/Automobiliu-nuoma-Toyota-Avensis-autonuoma-265x130.jpg",
+    title: "Toyota Avensis",
+    size: "Standartinė (SWMR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 22.45€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 12,
+    imgSrc:
+      "../../public/auto/Automobiliu-nuoma-Toyota-CHR-autonuoma.-265x156.jpg",
+    title: "Toyota CHR",
+    size: "Vidutinė (IFAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 25.98€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 13,
+    imgSrc:
+      "../../public/auto/Automobiliu-nuoma-Toyota-Corolla-autonuoma-11-265x135.jpg",
+    title: "Toyota Corolla",
+    size: "Vidutinė (IWAR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 24.99€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 14,
+    imgSrc:
+      "../../public/auto/Automobiliu-nuoma-Toyota-Yaris-autonuoma.-260x160.jpg",
+    title: "Toyota Yaris",
+    size: "Ekonominė",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 14.56€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 15,
+    imgSrc:
+      "../../public/auto/car_group_2018-05-15-22-29-46_5afb5f5a2c54e-1-265x153.jpg",
+    title: "Ford Focus",
+    size: "Kompaktinė (CDAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 21.56€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 16,
+    imgSrc: "../../public/auto/Ford-Fiesta.png",
+    title: "Ford Fiesta",
+    size: "Ekonominė",
+    year: "metai: 2020",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 17.67€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 17,
+    imgSrc: "../../public/auto/Ford-Focus-EU-1-265x151.png",
+    title: "Ford Focus",
+    size: "Kompaktinė (CDAR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 13,86€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 18,
+    imgSrc: "../../public/auto/Hyundai-i10.jpg",
+    title: "Hyundai i10",
+    size: "Mini (MDMR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 14.55€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 19,
+    imgSrc: "../../public/auto/VW-Taigo.jpg",
+    title: "VW Taigo",
+    size: "Kompaktinė (CFAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 24€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 20,
+    imgSrc: "../../public/auto/hyundai-i30.png",
+    title: "Hyundai i30",
+    size: "Kompaktinė (CDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 18.88€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 21,
+    imgSrc: "../../public/auto/hyundai-i40-265x111.png",
+    title: "hyundai i40",
+    size: "Standartinė (SWAR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 24.66€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 22,
+    imgSrc: "../../public/auto/i40-new-265x140.jpg",
+    title: "hyundai i40 CW",
+    size: "Standartinė (CWAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 24.66€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 23,
+    imgSrc: "../../public/auto/Mondeo.png",
+    title: "Ford Mondeo",
+    size: "Standartinė (CDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 27.55€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 24,
+    imgSrc: "../../public/auto/Toyota-Auris.jpg",
+    title: "Toyota-Auris",
+    size: "Kompaktinė (CDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 19.77€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 25,
+    imgSrc: "../../public/auto/Toyota-Corolla.jpg",
+    title: "Toyota Corolla New",
+    size: "Vidutinė (IDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 19.99€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 26,
+    imgSrc: "../../public/auto/toyota-rav4.jpg",
+    title: "Toyota RAV 4",
+    size: "Visureigis (FDAR)",
+    year: "metai: 2021",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 29.99€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 27,
+    imgSrc: "../../public/auto/Volkswagen-Passsat.png",
+    title: "Volkswagen Passsat",
+    size: "Standartinė (SDMR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 26.56€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 28,
+    imgSrc: "../../public/auto/volkswagen-t-roc.jpg",
+    title: "Volkswagen T-Roc A/T",
+    size: "Vidutinė (IFAR)",
+    year: "metai: 2023",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 28.88€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 29,
+    imgSrc: "../../public/auto/Volkswagen-Touran-.png",
+    title: "Volkswagen Touran",
+    size: "Vidutinė (IWMR)",
+    year: "metai: 2019",
+    info: "Pavarų dėžė: mechaninė",
+    price: "Kaina nuo: 22.23€",
+    reserve: "Rezervuoti",
+  },
+  {
+    id: 30,
+    imgSrc: "../../public/auto/VW-T-Cross.jpg",
+    title: "VW-T-Cross",
+    size: "Vidutinė (CFAR)",
+    year: "metai: 2022",
+    info: "Pavarų dėžė: automatinė",
+    price: "Kaina nuo: 24€",
+    reserve: "Rezervuoti",
+  },
+];
+
+const CarCard = ({ car, isFlipped, onClick }) => (
+  <div className={`car-card ${isFlipped ? "flipped" : ""}`} onClick={onClick}>
+    <div className="car-card-inner">
+      <div className="car-card-front">
+        <img src={car.imgSrc} alt={car.title} />
+        <h2>{car.title}</h2>
+      </div>
+      <div className="car-card-back">
+        <h2>{car.title}</h2>
+        <p>{car.size}</p>
+        <p>{car.year}</p>
+        <p>{car.info}</p>
+        <p>{car.price}</p>
+        <a href="/rezervuoti">{car.reserve}</a>
+      </div>
+    </div>
+  </div>
+);
+
 const Page = () => {
+  const [flippedCards, setFlippedCards] = useState({});
+
+  const handleCardClick = (id) => {
+    setFlippedCards((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <>
       <h1>Automobiliai nuomai</h1>
       <div className="autos">
-        <div className="auto1">
-          <img
-            src="../../public/auto/Automobilio-nuoma-Toyota-Corolla-autonuoma-Vilniuje3-265x128.jpg"
-            alt="auto1"
+        {cars.map((car) => (
+          <CarCard
+            key={car.id}
+            car={car}
+            isFlipped={flippedCards[car.id]}
+            onClick={() => handleCardClick(car.id)}
           />
-          <h2>Toyota-Corolla</h2>
-          <p>Toyota-Corolla ...</p>
-        </div>
-        <div className="auto2">
-          <img
-            src="../../public/auto/Automobilio-nuoma-Volkswagen-Golf3-265x151.png"
-            alt="auto2"
-          />
-          <h2>Volkswagen-Golf3</h2>
-          <p>Volkswagen ...</p>
-        </div>
-        <div className="auto3">
-          <img
-            src="../../public/auto/Automobilio-nuoma-Volkswagen-Jetta-265x132.png"
-            alt="auto3"
-          />
-          <h2>Volkswagen-Jetta</h2>
-          <p>Volkswagen-Jetta ...</p>
-        </div>
-        <div className="auto4">
-          <img
-            src="../../public/auto/Automobilio-nuoma-Volkswagen-Polo-265x149.png"
-            alt="auto4"
-          />
-          <h2>Volkswagen-Polo</h2>
-          <p>Volkswagen-Polo ...</p>
-        </div>
-        <div className="auto5">
-          <img
-            src="../../public/auto/Automobilių-nuoma-Ford-Focus3-265x144.png"
-            alt="auto5"
-          />
-          <h2>Ford-Focus</h2>
-          <p>Ford-Focus ...</p>
-        </div>
-        <div className="auto6">
-          <img
-            src="../../public/auto/Automobilių-nuoma-Ford-Mondeo.-265x153.png"
-            alt="auto6"
-          />
-          <h2>Ford-Mondeo</h2>
-          <p>Ford-Mondeo ...</p>
-        </div>
-        <div className="auto7">
-          <img
-            src="../../public/auto/Automobilių-nuoma-Hyundai-i10-252x160.jpg"
-            alt="auto7"
-          />
-          <h2>Hyundai-i10</h2>
-          <p>Hyundai-i10 ...</p>
-        </div>
-        <div className="auto8">
-          <img src="../../public/auto/hyundai-i20.png" alt="auto8" />
-          <h2>hyundai-i20</h2>
-          <p>hyundai-i20 ...</p>
-        </div>
-        <div className="auto9">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Hyundai-i30.-265x138.jpg"
-            alt="auto9"
-          />
-          <h2>Hyundai-i30</h2>
-          <p>Hyundai-i30 ...</p>
-        </div>
-        <div className="auto10">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Toyota-Auris-autonuoma..-265x150.jpg"
-            alt="auto10"
-          />
-          <h2>Toyota-Auris</h2>
-          <p>Toyota-Auris ...</p>
-        </div>
-        <div className="auto11">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Toyota-Avensis-autonuoma-265x130.jpg"
-            alt="auto11"
-          />
-          <h2>Toyota-Avensis</h2>
-          <p>Toyota-Avensis ...</p>
-        </div>
-        <div className="auto12">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Toyota-CHR-autonuoma.-265x156.jpg"
-            alt="auto12"
-          />
-          <h2>Toyota-CHR</h2>
-          <p>Toyota-CHR ...</p>
-        </div>
-        <div className="auto13">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Toyota-Corolla-autonuoma-11-265x135.jpg"
-            alt="auto13"
-          />
-          <h2>Toyota-Corolla</h2>
-          <p>Toyota-Corolla ...</p>
-        </div>
-        <div className="auto14">
-          <img
-            src="../../public/auto/Automobiliu-nuoma-Toyota-Yaris-autonuoma.-260x160.jpg"
-            alt="auto14"
-          />
-          <h2>Toyota-Yaris</h2>
-          <p>Toyota-Yaris ...</p>
-        </div>
-        <div className="auto15">
-          <img
-            src="../../public/auto/car_group_2018-05-15-22-29-46_5afb5f5a2c54e-1-265x153.jpg"
-            alt="auto15"
-          />
-          <h2>Ford Focus</h2>
-          <p>Ford Focus ....</p>
-        </div>
-        <div className="auto16">
-          <img src="../../public/auto/Ford-Fiesta.png" alt="auto16" />
-          <h2>Ford-Fiesta</h2>
-          <p>Ford-Fiesta.png</p>
-        </div>
-        <div className="auto17">
-          <img
-            src="../../public/auto/Ford-Focus-EU-1-265x151.png"
-            alt="auto17"
-          />
-          <h2>Ford Focus</h2>
-          <p>Ford Focus ...</p>
-        </div>
-        <div className="auto18">
-          <img src="../../public/auto/Hyundai-i10.jpg" alt="auto18" />
-          <h2>Hyundai-i10</h2>
-          <p>Hyundai-i10</p>
-        </div>
-        <div className="auto19">
-          <img src="../../public/auto/hyundai-i20.png" alt="auto19" />
-          <h2>Opel</h2>
-          <p>Opelas yra geriausias automobilis</p>
-        </div>
-        <div className="auto20">
-          <img src="../../public/auto/hyundai-i30.png" alt="auto20" />
-          <h2>Hyundai-i30</h2>
-          <p>Hyundai-i10</p>
-        </div>
-        <div className="auto21">
-          <img src="../../public/auto/hyundai-i40-265x111.png" alt="auto21" />
-          <h2>hyundai-i40</h2>
-          <p>hyundai-i40</p>
-        </div>
-        <div className="auto22">
-          <img src="../../public/auto/i40-new-265x140.jpg" alt="auto22" />
-          <h2>hyundai-i40 new</h2>
-          <p>hyundai-i40 new ...</p>
-        </div>
-        <div className="auto23">
-          <img src="../../public/auto/Mondeo.png" alt="auto23" />
-          <h2>Ford Mondeo</h2>
-          <p>Ford Mondeo ...</p>
-        </div>
-        <div className="auto24">
-          <img src="../../public/auto/Toyota-Auris.jpg" alt="auto24" />
-          <h2>Toyota-Auris</h2>
-          <p>Toyota-Auris ...</p>
-        </div>
-        <div className="auto25">
-          <img src="../../public/auto/Toyota-Corolla.jpg" alt="auto25" />
-          <h2>Toyota-Corolla</h2>
-          <p>Toyota-Corolla ...</p>
-        </div>
-        <div className="auto26">
-          <img src="../../public/auto/toyota-rav4.jpg" alt="auto26" />
-          <h2>toyota-rav</h2>
-          <p>toyota-rav4.jpg ...</p>
-        </div>
-        <div className="auto27">
-          <img src="../../public/auto/Volkswagen-Passsat.png" alt="auto27" />
-          <h2>Volkswagen-Passsat</h2>
-          <p>Volkswagen-Passsat ...</p>
-        </div>
-        <div className="auto28">
-          <img src="../../public/auto/volkswagen-t-roc.jpg" alt="auto28" />
-          <h2>volkswagen-t-roc</h2>
-          <p>volkswagen-t-roc ...</p>
-        </div>
-        <div className="auto29">
-          <img src="../../public/auto/Volkswagen-Touran-.png" alt="auto29" />
-          <h2>Volkswagen-Touran</h2>
-          <p>Volkswagen-Touran</p>
-        </div>
-        <div className="auto30">
-          <img src="../../public/auto/VW-T-Cross.jpg" alt="auto30" />
-          <h2>VW-T-Cross</h2>
-          <p>VW-T-Cross ...</p>
-        </div>
+        ))}
       </div>
     </>
   );
