54fb781d047568d7940f9921b573ac582bb28edd
dgenutis <dainiusgenutis@yahoo.com>
2024-08-13 13:08:58 +0300

    nuotraukos

---
+++ src/components/Page.jsx
@@ -0,0 +1,162 @@
+import './Page.css'
+const Page = () => {
+  return (
+    <>
+<h1>Automobiliai nuomai</h1>
+<div className="autos">
+    <div className='auto1'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto1' />
+        <h2>Hyundai</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto2'>
+        <img src='../../public/auto/hyundai-i30.png' alt='auto2' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto3'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto3' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto4'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto4' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto5'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto5' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto6'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto6' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto7'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto7' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto8'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto8' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto9'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto9' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto10'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto10' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto11'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto11' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto12'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto12' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto13'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto13' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto14'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto14' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto15'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto15' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto16'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto16' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto17'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto17' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto18'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto18' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto19'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto19' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto20'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto20' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto21'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto21' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto22'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto22' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto23'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto23' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto24'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto24' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto25'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto25' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto26'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto26' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto27'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto27' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto28'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto28' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto29'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto29' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+    <div className='auto30'>
+        <img src='../../public/auto/hyundai-i20.png' alt='auto30' />
+        <h2>Opel</h2>
+        <p>Opelas yra geriausias automobilis</p>
+    </div>
+</div>
+    </>
+  );
+};
+
+export default Page;
