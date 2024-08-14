ad84452efd4c65fe31b245ff59b2b549a4bba62d
Petras Butkus <p.butkus360@gmail.com>
2024-08-14 15:49:38 +0300

    refactor CarCard

---
+++ src/components/CarCard/CarCard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const CarCard = ({ car }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  return (
+    <div
+      className={`car-card ${isFlipped ? "flipped" : ""}`}
+      onClick={() => setIsFlipped((prev) => !prev)}
+    >
+      <div className="car-card-inner">
+        <div className="car-card-front">
+          <img src={car.imgSrc} alt={car.title} />
+          <h2>{car.title}</h2>
+        </div>
+        <div className="car-card-back">
+          <h2>{car.title}</h2>
+          <p>{car.size}</p>
+          <p>{car.year}</p>
+          <p>{car.info}</p>
+          <p>{car.price}</p>
+          <a href="/rezervuoti" className="btn btn-primary">
+            {car.reserve}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CarCard.propTypes = {
+  car: PropTypes.shape({
+    imgSrc: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    size: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    info: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    reserve: PropTypes.string.isRequired,
+  }).isRequired,
+  isFlipped: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default CarCard;
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -6,61 +6,19 @@ import "./Index.css";
 import { cars } from "../../data/mockdata.json";
 import PropTypes from "prop-types";
 import Search from "../../components/Search";
+import CarCard from "../../components/CarCard/CarCard";
 
 const getRandomCars = (cars) => {
   const shuffled = cars.sort(() => 0.5 - Math.random());
   return shuffled.slice(0, 10);
 };
 
-const CarCard = ({ car, isFlipped, onClick }) => (
-  <div className={`car-card ${isFlipped ? "flipped" : ""}`} onClick={onClick}>
-    <div className="car-card-inner">
-      <div className="car-card-front">
-        <img src={car.imgSrc} alt={car.title} />
-        <h2>{car.title}</h2>
-      </div>
-      <div className="car-card-back">
-        <h2>{car.title}</h2>
-        <p>{car.size}</p>
-        <p>{car.year}</p>
-        <p>{car.info}</p>
-        <p>{car.price}</p>
-        <a href="/rezervuoti" className="btn btn-primary">
-          {car.reserve}
-        </a>
-      </div>
-    </div>
-  </div>
-);
-
-CarCard.propTypes = {
-  car: PropTypes.shape({
-    imgSrc: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    size: PropTypes.string.isRequired,
-    year: PropTypes.string.isRequired,
-    info: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    reserve: PropTypes.string.isRequired,
-  }).isRequired,
-  isFlipped: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
 const CarCarousel = ({ cars }) => {
-  const [flippedCards, setFlippedCards] = useState({});
   const [randomCars, setRandomCars] = useState([]);
 
   useEffect(() => {
     setRandomCars(getRandomCars(cars));
   }, [cars]);
-
-  const handleCardClick = (id) => {
-    setFlippedCards((prev) => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
-  };
 
   const settings = {
     dots: true,
@@ -94,13 +52,7 @@ const CarCarousel = ({ cars }) => {
     <div className="carousel-container">
       <Slider {...settings}>
         {randomCars.map((car) => (
-          <div key={car.id} onClick={() => handleCardClick(car.id)}>
-            <CarCard
-              car={car}
-              isFlipped={flippedCards[car.id] || false}
-              onClick={() => handleCardClick(car.id)}
-            />
-          </div>
+          <CarCard key={car.id} car={car} />
         ))}
       </Slider>
     </div>
