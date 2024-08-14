2e818c9af87de2e9063f83e79acd8b0e6be5cd97
PauliusDEG <paulius.degutis1@gmail.com>
2024-08-14 10:01:59 +0200

    trecias

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="address">
+          <h3>Contact Us</h3>
+          <p></p>
+          <p></p>
+          <p>Email: contact@indexRental.lt</p>
+          <p>Number: 86034564</p>
+        </div>
+        <div className="social-media">
+          <h3>Follow Us on:</h3>
+          <div className="social-icons">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaTwitter />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
