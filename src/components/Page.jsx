5847ec3fbb1a4124c434e104fc175c4ff056e302
Mantanavicius <mlantanavicius@gmail.com>
2024-08-14 13:55:53 +0300

    structure changes

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -381,7 +381,6 @@ const CarCarousel = ({ cars }) => {
     </Slider>
   );
 };
-
 
 const Page = () => {
   return (
