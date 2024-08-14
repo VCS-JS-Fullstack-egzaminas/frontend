36fcc686dd3cb8b6227dc7aceac4d30374127a1c
dgenutis <dainiusgenutis@yahoo.com>
2024-08-14 15:15:29 +0300

    pakeitimai

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,15 @@
 import "./Search.css";
+import Button from "../../components/ui/Button";
 
 const Search = () => {
   return (
     <div className="search-main">
       <div className="banner-container">
         <h1>Drive in style, Rent with smiles</h1>
-        <div className="banner-form"></div>
+        <div className="banner-form">
+            <Button>spausk</Button>
+        </div>
+        
       </div>
     </div>
   );
