bbb796cb0cbe0c809894725a573851a43631d671
Mantanavicius <mlantanavicius@gmail.com>
2024-08-16 10:44:05 +0300

    Admin panel structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import MainLayout from "./layouts/MainLayout";
 import Index from "./pages/Index/Index";
-import NewListing from "./pages/NewListing/NewListing";
+import NewListing from "./components/Admin/Listings/NewListing/NewListing";
 import Error from "./pages/Error/Error";
 import ComponentsDemo from "./pages/ComponentsDemo/ComponentsDemo";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import ImageUpload from "./pages/ImageUpload/ImageUpload";
-
+import AdminLayout from "./layouts/AdminLayout";
+import Listings from "./components/Admin/Listings/Listings";
+import Reservations from "./components/Admin/Reservations/Reservations";
+import Users from "./components/Admin/Users/Users";
 
 const router = createBrowserRouter([
   {
@@ -19,10 +22,6 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
-      },
-      {
-        path: "/new-listing",
-        element: <NewListing />,
       },
     ],
   },
@@ -41,6 +40,30 @@ const router = createBrowserRouter([
   {
     path: "/dev/image-upload-demo",
     element: <ImageUpload />,
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "/listings",
+        element: <Listings />,
+        children: [
+          {
+            path: "/new-listing",
+            element: <NewListing />,
+          },
+        ],
+      },
+      {
+        path: "/reservations",
+        element: <Reservations />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
   },
 ]);
 
