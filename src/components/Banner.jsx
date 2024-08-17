9c7473b90a996364de33e18fc4e1350d31ffa26a
Petras Butkus <p.butkus360@gmail.com>
2024-08-17 09:59:38 +0300

    change font and minor redesign of Banner, Comentators and Footer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,23 @@
-import './Banner.css'
+import "./Banner.css";
 const Banner = () => {
   return (
     <>
-      <div className="banner-container2">
+      <div className="flex items-center mt-24 bg-ecstasy-500 shadow-md">
         <div className="banner-img">
           <img src="../../public/final-1.webp" alt="" />
         </div>
         <div className="banner-message">
-          <div className="message-star">
+          <div className="text-white text-3xl">
             <span>&#9733;</span>
             <span>&#9733;</span>
             <span>&#9733;</span>
             <span>&#9733;</span>
             <span>&#9733;</span>
           </div>
-
-          <h3>
-            Drive in style, rent with a smile – that’s the VCS Rental way.
+          <h3 className="font-bold text-4xl max-w-96 text-white">
+            Drive in style, rent with a smile - that&apos;s the VCS Rental way.
           </h3>
-          <p>
+          <p className="max-w-96 text-white/90">
             Premium Service - Competitive Pricing - Diverse Fleet Selection -
             Professional Excellence
           </p>
