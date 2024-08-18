0d3a33ffd286bb6a51971452036369f03ce634df
dgenutis <dainiusgenutis@yahoo.com>
2024-08-18 12:52:58 +0300

    fleet su filtracija

--- src/components/Fleet.jsx
+++ src/components/Fleet.jsx
@@ -4,26 +4,51 @@ import { cars } from "../data/mockdata.json";
 
 const Fleet = () => {
   const [carList, setCarList] = useState([]);
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [fadeClass, setFadeClass] = useState("fade-in");
 
   useEffect(() => {
-   
     setCarList(cars);
+    setFilteredCars(cars);
   }, []);
+
+  const filterCars = (size) => {
+    setFadeClass("fade-out");
+    setTimeout(() => {
+      if (size === "All") {
+        setFilteredCars(carList);
+      } else {
+        const filtered = carList.filter((car) => car.size.includes(size));
+        setFilteredCars(filtered);
+      }
+      setFadeClass("fade-in");
+    }, 500);
+  };
 
   return (
     <>
       <div className="fleet-Container1">
         <h2>
-          Discover the Perfect Ride: Introducing the VCS Rental Fleet in Lithuania
+          Discover the Perfect Ride: Introducing the VCS Rental Fleet in
+          Lithuania
         </h2>
+        <div className="filter-buttons">
+          <button onClick={() => filterCars("All")}>All</button>
+          <button onClick={() => filterCars("Mini")}>Mini</button>
+          <button onClick={() => filterCars("Economic")}>Economic</button>
+          <button onClick={() => filterCars("Compact")}>Compact</button>
+          <button onClick={() => filterCars("Medium")}>Medium</button>
+          <button onClick={() => filterCars("Standart")}>Standart</button>
+          <button onClick={() => filterCars("SUV")}>SUV</button>
+        </div>
       </div>
-      <div className="fleet-Container2">
-        {carList.map((car) => (
+      <div className={`fleet-Container2 ${fadeClass}`}>
+        {filteredCars.map((car) => (
           <div key={car.id} className="fleet">
             <img src={car.imgSrc} alt={car.name} className="fleet-img" />
             <div className="fleet-details">
               <h3>{car.title}</h3>
-              <p> {car.size}</p>
+              <p>{car.size}</p>
               <p>{car.year}</p>
               <p>{car.price}</p>
               <a href="/rezervuoti" className="btn btn-primary">
