67640e15696eecf2e4575060c3ee2d54abe1f278
Petras Butkus <p.butkus360@gmail.com>
2024-08-18 21:52:15 +0300

    admin panel authprovider impl (redirect needs fixing)

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,7 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import AdminHeader from "../pages/Admin/AdminHeader/AdminHeader";
+import { useAuth } from "../hooks/useAuth";
+import { useEffect } from "react";
 
 const AdminLayout = () => {
+  // TODO - fix redirect out of admin panel if role !== "admin"
+  // const { user, role } = useAuth();
+
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   console.log(role, user);
+  // }, [role]);
+
   return (
     <div className="flex flex-col h-screen">
       <AdminHeader />
