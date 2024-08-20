8d78f496780e0fcff2a4693ef9acc09e416d655f
Petras Butkus <p.butkus360@gmail.com>
2024-08-20 12:07:59 +0300

    fix NewListing upload logic (DO NOT CHANGE)

--- src/pages/Admin/NewListing/NewListing.jsx
+++ src/pages/Admin/NewListing/NewListing.jsx
@@ -1,12 +1,14 @@
+import "./NewListing.css";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createListing } from "../../../services/listingsService";
 import { uploadImg } from "../../../services/uploadService";
-import Button from "../../../components/ui/Button";
 
 const NewListing = () => {
   const [entryData, setEntryData] = useState("");
   const [images, setImages] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const titleRef = useRef();
   const descriptionRef = useRef();
@@ -19,8 +21,6 @@ const NewListing = () => {
   const yearRef = useRef();
   const sizeRef = useRef();
 
-  const inputStyle = "mb-2 p-2 rounded-md border border-gray-300";
-
   const handleImageInput = (e) => {
     const newImages = Array.from(e.target.files).map((file) => ({
       file,
@@ -28,65 +28,99 @@ const NewListing = () => {
       id: crypto.randomUUID(),
     }));
     setImages((prev) => [...prev, ...newImages]);
+    console.log(newImages);
   };
 
   const handleImageDelete = (id) => {
     setImages((prev) => prev.filter((image) => image.id !== id));
   };
+
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(e);
+    setLoading(true);
+
     try {
       const imagesFormData = new FormData();
-      images.forEach((image) => imagesFormData.append("images", image.file));
+      images.forEach((image) => {
+        imagesFormData.append("images", image.file);
+      });
 
       const uploadedImagesResponse = await uploadImg(imagesFormData);
 
+      console.log(uploadedImagesResponse);
+
       await createListing({
         ...entryData,
-        photos: uploadedImagesResponse.data.images,
+        photos: uploadedImagesResponse.data.urls,
       });
-
-      alert("Record successfully added");
+      console.log("bando ikelti", entryData);
+      alert("Record succesfully added");
+      setLoading(false);
       navigate("/admin/listings");
     } catch (error) {
-      console.error(error);
+      console.log(error);
+      setLoading(false);
     }
+
+    console.log("returning");
   };
 
-  const handleInputChange = () => {
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    let laikinasTitle = titleRef.current.value;
+    let laikinasDescription = descriptionRef.current.value;
+    let laikinasPrice = priceRef.current.value;
+    let laikinasAvailable = availableRef.current.value;
+    let laikinasMaxDur = maxDurationRef.current.value;
+    let laikinasMinDur = minDurationRef.current.value;
+    let laikinasExtras = extrasRef.current.value;
+    let laikinasYear = yearRef.current.value;
+    let laikinasSize = sizeRef.current.value;
+
+    let size = laikinasSize;
+    let year = laikinasYear;
+    let title = laikinasTitle;
+    let description = laikinasDescription;
+    let price = laikinasPrice;
+    let available = laikinasAvailable;
+    let min_duration = laikinasMinDur;
+    let max_duration = laikinasMaxDur;
+    let extras = laikinasExtras;
     setEntryData({
-      title: titleRef.current.value,
-      description: descriptionRef.current.value,
-      price: priceRef.current.value,
-      available: availableRef.current.value,
-      min_duration: minDurationRef.current.value,
-      max_duration: maxDurationRef.current.value,
-      extras: extrasRef.current.value,
-      year: yearRef.current.value,
-      size: sizeRef.current.value,
+      ...entryData,
+      title,
+      description,
+      price,
+      available,
+      min_duration,
+      max_duration,
+      extras,
+      photos,
+      year,
+      size,
     });
   };
 
   return (
-    <div className="p-4 border border-red-100 rounded-md max-w-md mx-auto mt-4">
+    <div className="new-listing-container card">
       <h2 className="font-bold text-3xl mb-4">Add New Rental</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="new-listing-form gap-1">
         <input
-          className={inputStyle}
+          className="input-field"
           ref={titleRef}
           type="text"
           onChange={handleInputChange}
           placeholder="Title"
         />
         <input
-          className={inputStyle}
+          className="input-field"
           ref={yearRef}
           type="number"
           onChange={handleInputChange}
           placeholder="Year"
         />
         <select
-          className={inputStyle}
+          className="input-field"
           onChange={handleInputChange}
           ref={sizeRef}
         >
@@ -98,83 +132,88 @@ const NewListing = () => {
           <option value="SUV">SUV</option>
         </select>
         <textarea
-          className={inputStyle}
+          className="input-field"
           ref={descriptionRef}
           type="text"
           onChange={handleInputChange}
           placeholder="Description"
         />
         <input
-          className={inputStyle}
+          className="input-field"
           ref={priceRef}
           type="number"
           onChange={handleInputChange}
           placeholder="Price"
         />
         <select
-          className={inputStyle}
+          className="input-field"
           onChange={handleInputChange}
           ref={availableRef}
         >
           <option value="true">Available</option>
           <option value="false">Unavailable</option>
         </select>
+        {/* <input  placeholder="Photo URLs" className="input-field" ref={photosRef} type="text" onChange={handleInputChange} /> */}
         <div>
-          {images.map((image, index) => (
-            <div key={index} className="mb-2 relative">
-              <img
-                src={image.preview}
-                alt={`Thumbnail ${index}`}
-                className="w-full h-auto rounded-md"
-              />
-              <div
-                className="absolute top-2 right-2 h-6 w-6 bg-red-600 text-white rounded-full hover:bg-red-700 transition-bg duration-150 flex items-center justify-center cursor-pointer"
-                onClick={() => handleImageDelete(image.id)}
-              >
-                ×
+          {/* <form className="grid gap-4" onSubmit={handleSubmit}> */}
+          <div>
+            {images.map((image, index) => (
+              <div key={index}>
+                <img src={image.preview} alt={`Thumbnail ${index}`} />
+                <button
+                  type="button"
+                  onClick={() => handleImageDelete(image.id)}
+                >
+                  x
+                </button>
               </div>
+            ))}
+            <div className="input-field">
+              <label htmlFor="file-input">
+                <span className="mt-2 text-sm text-gray-500">Add image</span>
+              </label>
+              <input
+                id="file-input"
+                type="file"
+                multiple
+                onChange={handleImageInput}
+                className="hidden"
+                accept="image/*"
+                ref={photosRef}
+              />
             </div>
-          ))}
-          <div className={inputStyle}>
-            <label htmlFor="file-input" className="">
-              Add image
-            </label>
-            <input
-              id="file-input"
-              name="file-input"
-              type="file"
-              multiple
-              onChange={handleImageInput}
-              accept="image/*"
-              ref={photosRef}
-            />
           </div>
         </div>
         <input
           placeholder="Min Rental Duration"
-          className={inputStyle}
+          className="input-field"
           ref={minDurationRef}
           type="number"
           onChange={handleInputChange}
-          min={1}
         />
         <input
           placeholder="Max Rental Duration"
-          className={inputStyle}
+          className="input-field"
           ref={maxDurationRef}
           type="number"
           onChange={handleInputChange}
-          min={1}
         />
         <input
           placeholder="Extras"
-          className={inputStyle}
+          className="input-field"
           ref={extrasRef}
           type="text"
           onChange={handleInputChange}
         />
-        <Button type="submit">Add Listing</Button>
+        <button
+          type="submit"
+          className="btn btn-primary w-full disabled:bg-ecstasy-800"
+          disabled={loading}
+        >
+          {loading ? "Adding listing..." : "Add listing"}
+        </button>
       </form>
+      <div></div>
     </div>
   );
 };
