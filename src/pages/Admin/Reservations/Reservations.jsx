3cdb8ea6be70a3788187974b2201f66b760d3bc4
Mantanavicius <mlantanavicius@gmail.com>
2024-08-21 15:25:56 +0300

    reservations style changes

--- src/pages/Admin/Reservations/Reservations.jsx
+++ src/pages/Admin/Reservations/Reservations.jsx
@@ -1,48 +1,45 @@
 import { Helmet } from "react-helmet";
-import { useEffect,  useState, useRef  } from "react";
-import {  useNavigate } from "react-router-dom";
-import {getAllReservations, updateReservationById}
- from "../../../services/reservationsService";
+import { useEffect, useState, useRef } from "react";
+import {
+  getAllReservations,
+  updateReservationById,
+} from "../../../services/reservationsService";
+import Button from "../../../components/ui/Button";
 
 const Reservations = () => {
-  const [reservations, setReservations] = useState({});
+  const [reservations, setReservations] = useState([]);
   const [showEdit, setShowEdit] = useState(false);
-  const [editData, setEditData] = useState({});
-  const [editId, setEditId] = useState('')
+  const [editData, setEditData] = useState([]);
+  const [editId, setEditId] = useState("");
   const [notification, setNotification] = useState(null);
-  const navigate = useNavigate();
   const statusRef = useRef();
-
 
   useEffect(() => {
     const getReservations = async () => {
       try {
         const response = await getAllReservations();
         setReservations(response.data);
-        console.log(response.data)
-
       } catch (error) {
         console.error("Error Fetching Reservation", error);
       }
     };
     getReservations();
-  }, []); //isveda visus reservations
-  
+  }, [showEdit]);
+
   const displayNotification = (type, message) => {
     setNotification({ type, message });
-    setTimeout(() => navigate("/admin/reservations"), 3000);
-  }; 
-
-  const handleInputChange = (e) => {
-    const status = e.target.value
-    setEditData({status});
+    setTimeout(() => setNotification(null), 3000);
   };
 
+  const handleInputChange = (e) => {
+    const status = e.target.value;
+    setEditData({ status });
+  };
 
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
-      console.log(editData,editId)
+      console.log(editData, editId);
       await updateReservationById(editId, editData);
       displayNotification("success", "Record successfully updated!");
       setShowEdit(false);
@@ -57,77 +54,101 @@ const Reservations = () => {
         <title>Reservation</title>
       </Helmet>
 
-      <div>
-      {reservations.length > 0 ? (
-          <div className="space-y-4 flex flex-col justify-center items-center">
-            {reservations.map((info) => (
-              <div
-                key={info._id}
-                className="bg-white shadow-md rounded-md p-6 flex flex-col justify-center items-center flex-wrap gap-4 w-fit justify-self-center md:flex md:flex-row md:justify-between md:w-full">
-                <div className="details">
-                  <p className="mb-2" >
-                  <strong> Listing: </strong>{info.listing.title}</p>
-                  <p className="mb-2">
-                    <strong>User:</strong> {info.user.email}
-                  </p>
-                  <p className="mb-2">
-                  <strong>Start:</strong>{" "}
-                  {new Date(info.start).toLocaleString()}
-                  </p>
-                  <p className="mb-2">
-                  <strong>End:</strong> {" "}
-                  {new Date(info.end).toLocaleString()}
-                  </p>
-                  <p className="mb-2">
-                    <strong>Status:</strong> {info.status} 
-                  </p>
-                  <button onClick={() => {
-                            setShowEdit(true);
-                             setEditId(info._id);
-                             }}>Edit</button>
-                  <p className="mb-2">
-                    <strong>Reservation created at:</strong> 
-                    {" "}
-                  {new Date(info.createdAt).toLocaleString()}
-                  </p>
-                  <p className="mb-2">
-                    <strong>Reservation updated at:</strong> 
-                    {" "}
-                    {new Date(info.updatedAt).toLocaleString()}
-                  </p>
-              
+      <div className="p-8 bg-gray-100 min-h-screen">
+        <div className="max-w-screen-lg mx-auto bg-white shadow-md rounded-md p-8">
+          {reservations.length > 0 ? (
+            <div className="space-y-4">
+              {reservations.map((info) => (
+                <div
+                  key={info._id}
+                  className="bg-white shadow-md rounded-md p-6 flex flex-col md:flex-row md:justify-between md:items-center gap-4 relative"
+                >
+                  <div className="details">
+                  <img src={info.listing.photos} alt="car" className="w-32  top-6 right-6 rounded-md mb-8"/>
+                    <p className="mb-2">
+                      <strong> Listing: </strong>
+                      {info.listing.title} <i>{info.listing.year}</i>
+                    </p>
+                    <p className="mb-2 break-words">
+                      <strong>User:</strong> {info.user.email}
+                    </p>
+                    <p className="mb-2">
+                      <strong>Start:</strong>{" "}
+                      {new Date(info.start).toLocaleString()}
+                    </p>
+                    <p className="mb-2">
+                      <strong>End:</strong>{" "}
+                      {new Date(info.end).toLocaleString()}
+                    </p>
+                    <p className="mb-2 capitalize">
+                      <strong>Status:</strong> {info.status}
+                    </p>
+                    <p className="mb-2">
+                      <strong>Reservation created at:</strong>{" "}
+                      {new Date(info.createdAt).toLocaleString()}
+                    </p>
+                    <p className="mb-2">
+                      <strong>Reservation updated at:</strong>{" "}
+                      {new Date(info.updatedAt).toLocaleString()}
+                    </p>
+                    <Button
+                      color="blue"
+                      onClick={() => {
+                        setShowEdit(true);
+                        setEditId(info._id);
+                      }}
+                    >
+                      Change Status
+                    </Button>
+                    {notification && editId === info._id && (
+                      <p
+                        className={`mt-4 ${
+                          notification.type === "success"
+                            ? "text-green-500"
+                            : "text-red-500"
+                        }`}
+                      >
+                        {notification.message}
+                      </p>
+                    )}
+                  </div>
+                  {editId === info._id && showEdit && (
+                    <form onSubmit={handleUpdate} className="mt-4">
+                      <div className="flex flex-col gap-2">
+                        <select
+                          onChange={(e) => handleInputChange(e)}
+                          ref={statusRef}
+                          value={editData.status}
+                          className="w-full p-2 border border-gray-300 rounded capitalize"
+                        >
+                          <option hidden defaultValue={info.status}>
+                            {info.status}
+                          </option>
+                          <option value="Pending">Pending</option>
+                          <option value="Accepted">Accepted</option>
+                          <option value="Declined">Declined</option>
+                        </select>
+                        <button
+                          type="submit"
+                          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  )}
                 </div>
-                {showEdit && (
-
-    <form onSubmit={handleUpdate}>
-      <div>
-      <select
-      ref={statusRef}
-      onChange={(e) => handleInputChange(e)}>
-      <option  hidden defaultValue={info.status}>{info.status}</option>
-      <option value="Pending">Pending</option>
-      <option value="Accepted">Accepted</option>
-      <option value="Declined">Declined</option>
-      </select>
+              ))}
+            </div>
+          ) : (
+            <p className="text-center text-gray-600">
+              No reservations available.
+            </p>
+          )}
+        </div>
       </div>
-      <button type="submit">Submit</button>
-      </form>
-
-        )}
-              </div>
-              
-            ))} </div>
-             
-        ) : (
-          <p>No reservations available.</p>
-        )}
-      
-
-      </div>
-
-
     </div>
   );
-}
+};
 
-export default Reservations
+export default Reservations;
