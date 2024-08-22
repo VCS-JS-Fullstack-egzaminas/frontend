5b68ff563c59f7e9de8e7dad231ea4f47476b0fd
Petras Butkus <p.butkus360@gmail.com>
2024-08-22 09:41:55 +0300

    handle edge cases

--- src/pages/MyReservations/MyReservations.jsx
+++ src/pages/MyReservations/MyReservations.jsx
@@ -2,19 +2,38 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { getMyReservations } from "../../services/reservationsService";
 import ReservationCarCard from "../../components/ReservationCarCard/ReservationCarCard";
+import { useNavigate } from "react-router-dom";
 
 const MyReservations = () => {
   const { user, loading } = useAuth();
   const [reservations, setReservations] = useState([]);
 
-  useEffect(() => {
-    const populateReservations = async () => {
-      const response = await getMyReservations();
-      setReservations(response.data);
-    };
+  const navigate = useNavigate();
 
-    populateReservations();
-  }, []);
+  if (!loading && !user) {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        navigate("/");
+      }
+    }
+  }, [loading, user, navigate]);
+
+  useEffect(() => {
+    if (user) {
+      const populateReservations = async () => {
+        const response = await getMyReservations();
+        setReservations(response.data);
+      };
+
+      populateReservations();
+    } else {
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   if (loading) {
     return <h1>loading</h1>;
@@ -26,14 +45,18 @@ const MyReservations = () => {
         <h1 className="text-left font-bold text-3xl mb-6">
           {user.username}&apos;s reservations
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 justify-center items-center">
-          {reservations.map((reservation) => (
-            <ReservationCarCard
-              key={reservation._id}
-              reservation={reservation}
-            />
-          ))}
-        </div>
+        {reservations.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 justify-center items-center">
+            {reservations.map((reservation) => (
+              <ReservationCarCard
+                key={reservation._id}
+                reservation={reservation}
+              />
+            ))}
+          </div>
+        ) : (
+          <h2>You don&apos;t have any reservations yet</h2>
+        )}
       </div>
     </div>
   );
